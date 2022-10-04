3183d023cb2790f2c0c165ac485c5a16fc59009e
piyush <100530138+Piyush-4254@users.noreply.github.com>
2022-10-04 19:22:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function Start()
 {
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/ckGjKiKv5/model.json",modelReady)
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/BTHXubbbi/model.json",modelReady);
 }
